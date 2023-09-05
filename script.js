7faba44ce0dd8e79c9f715aa0e9a4e7b1637b33a
sumanta kumar mohanta <mohantasumanta601@gmail.com>
2023-09-05 21:49:06 +0530

    loop over arrays and maps

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-const items = ['book','table','chair','kite'];
+const items = ["book", "table", "chair", "kite"];
 //iterate through normal for loop
-for(let i=0;i<items.length;i++){
-    console.log(items[i]);
+for (let i = 0; i < items.length; i++) {
+  console.log(items[i]);
 }
 
 // iterate through for of loop
 
-for(const item of items){
-    console.log(item);
+for (const item of items) {
+ console.log(item);
+}
+
+// iterate through array
+const users = [{ name: "happy" }, { name: "brad" }, { name: "steve" }];
+for(const user of users){
+   console.log(user.name);
+}
+
+//loop over string
+const str = 'hello world';
+for(const letter of str){
+   console.log(letter);
+}
+
+//loop over maps
+const map = new Map();
+map.set('name','happy');
+map.set('age',21);
+for(const [key,value]of map){
+    console.log(key,value);
 }
